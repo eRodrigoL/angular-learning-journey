efd8b3ad9c0ced4cfdf64b47d41c22c2f3e2d300
E Rodrigo Lopes <159098656+eRodrigoL@users.noreply.github.com>
2025-10-31 20:38:56 -0300

    feat(lab-08-14-components): gera componentes (toolbar, lifecycle e child)
    Cria três componentes:
    - components/toolbar:  menu lateral (sidenav) semântico.
    - pages/lifecycle: página de demonstração.
    - pages/lifecycle/child: componente filho para evidenciar hooks
    
    Comandos utilizados:
    ng g c componets/toolbar
    ng g c pages/lifecycle
    ng g c pages/lifecycle/child
    
    Refs: ANGULAR-LEARNING-JOURNEY, aulas-08-a-14

--- lab-08-14-components/src/app/app.ts
+++ lab-08-14-components/src/app/app.ts
@@ -1,11 +1,12 @@
+import { Toolbar } from './components/toolbar/toolbar';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toolbar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('lab-08-14-components');
---
+++ lab-08-14-components/src/app/pages/lifecycle/lifecycle.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle',
+  imports: [],
+  templateUrl: './lifecycle.html',
+  styleUrl: './lifecycle.scss',
+})
+export class Lifecycle {
+
+}
